2724fc70606ed0825d02775a0e13273cb2ed84a1
Borislav Adamovic <borislavadamovic9@gmail.com>
2020-04-17 14:08:43 +0200

    index.html

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-let form = document.querySelector('.form');
-let input = document.querySelector("#input");
-form.addEventListener('submit', event=>{
- event.preventDefault();
- let value = input.value;
- console.log(typeof value);
- 
-})
+(function () {
+ //varijable
+  let btns = document.querySelectorAll(".btn");
+  let deleteC = document.querySelector(".C");
+  let equal = document.querySelector(".equal");
+  let input = document.querySelector("#input");
+  //eventi
+  btns.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+      event.preventDefault();
+      let number = event.target.dataset.number;
+      input.value += number;
+    });
+  });
+  equal.addEventListener("click", (event) => {
+    let total = eval(input.value);
+    if (input.value === "") {
+      alert("Please enter valid values");
+    } else {
+      input.value = total;
+    }
+  });
+  deleteC.addEventListener("click", () => {
+    input.value = "";
+  });
+})();
